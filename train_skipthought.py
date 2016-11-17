0a4ea298ad016350f87d1dffcb3e4fd499dbeef1
yasufumi <yasufumi.taniguchi@gmail.com>
2016-11-18 04:00:18 +0900

    add bos token and eos token in toy dataset

--- train_skipthought.py
+++ train_skipthought.py
@@ -1,6 +1,7 @@
 import argparse
 
 from chainer import optimizers
+from chainer.optimizer import GradientClipping
 import chainer.functions as F
 import numpy as np
 
@@ -21,14 +22,17 @@ def parse_args():
     return parser.parse_args()
 
 def main(args):
-    text = ['We describe an approach for unsupervised learning of a generic, distributed sentence encoder.', 'Using the continuity of text from books, we train an encoder-decoder model that tries to reconstruct the surrounding sentences of an encoded passage.', 'Sentences that share semnatic and syntactic properties are thus mapped to similar vector representations.']
-    words = [text_to_word_sequence(sentence) for sentence in text]
-    vocab, _ = build_vocabulary(flatten(words), args.vocabulary)
+    text = ['We describe an approach for unsupervised learning of a generic distributed sentence encoder',
+            'Using the continuity of text from books we train an encoder-decoder model that tries to reconstruct the surrounding sentences of an encoded passage',
+            'Sentences that share semnatic and syntactic properties are thus mapped to similar vector representations']
+    words = [['<s>'] + text_to_word_sequence(sentence.lower()) + ['</s>'] for sentence in text]
+    vocab, id2word = build_vocabulary(flatten(words), args.vocabulary)
 
     skip_thought = SkipThought(len(vocab), args.embed, args.hidden)
     skip_thought.use_gpu(args.gpu)
     optimizer = optimizers.AdaGrad(lr=0.01)
     optimizer.setup(skip_thought)
+    optimizer.add_hook(GradientClipping(args.gradient_clipping))
 
     xp = skip_thought.xp
 
@@ -39,12 +43,19 @@ def main(args):
     source_sentence = F.transpose_sequence([xp.array([vocab[word] for word in words[1]], dtype=np.int32)])
     next_sentence = F.transpose_sequence([xp.array([vocab[word] for word in words[2]], dtype=np.int32)])
 
+    print(vocab['</s>'])
+    print(vocab['<s>'])
+
     for epoch in range(n_epoch):
         skip_thought.cleargrads()
         loss = skip_thought.forward_train(source_sentence, previous_sentence, next_sentence)
         print(loss.data)
         loss.backward()
         optimizer.update()
+        previous_prediction, next_prediction = skip_thought.forward_test(source_sentence, 50, vocab['<s>'], vocab['</s>'])
+        print(' '.join([id2word[id_[0]] for id_ in previous_prediction]))
+        print(' '.join([id2word[id_[0]] for id_ in next_prediction]))
+
 
 if __name__ == '__main__':
     args = parse_args()
