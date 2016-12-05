10a0bdd35f8135d297012101e0f7cb09447329da
yasufumi <yasufumi.taniguchi@gmail.com>
2016-12-06 04:57:57 +0900

    update batch.py

--- lib/batch.py
+++ lib/batch.py
@@ -6,9 +6,9 @@ def read_dataset(filename, eos_token):
         return [line.strip().split() + [eos_token] for line in f]
 
 def generate_batch(lines, vocab, batch_size, pad_id=-1):
-    max_length = max(len(line) for line in lines)
     sentences = iter([[vocab[w] for w in line] for line in lines])
     while length_hint(sentences) > 0:
-        filled_batch = [sentence + [pad_id] * (max_length - len(sentence) + 1)
-                            for sentence in islice(sentences, batch_size)]
+        batch = [sentence for sentence in islice(sentences, batch_size)]
+        max_length = max(len(b) for b in batch)
+        filled_batch = [b + [pad_id] * (max_length - len(b) + 1) for b in batch]
         yield list(zip(*filled_batch))
